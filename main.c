eccc1f232dc2cbe26b0538d920c77dda5288ddac
olahmed <149155192+olahmed@users.noreply.github.com>
2024-01-29 22:13:46 +0200

    Add files via upload
    signed char x = -6, y;
    y = x >> 2;
    what is the value of y now and How you find it ?

---
+++ main.c
@@ -0,0 +1,46 @@
+/**
+ ******************************************************************************
+ * @file           : Application.c
+ * @author         : ola ahmed
+ * @brief          : Contains the functionality of my application
+ ******************************************************************************
+ */
+
+/* ********************** Includes Section Start ********************** */
+    #include <stdio.h>
+    #include <stdlib.h>
+/* ********************** Includes Section End   ********************** */
+
+/* ********************** Macro Section Start ************************* */
+
+/* ********************** Macro Section End   ************************* */
+
+/* ******************* Global Variables Section Start ************** */
+
+/* ********************** Global Variables  Section End   ************* */
+
+/* ********************** Global Decleration Section Start ************* */
+
+/* ********************** Global Decleration  Section End   ************ */
+
+/* ********************** Sub-Program Declerations Section Start ******* */
+
+/* ********************** Sub-Program Declerations Section End ******* */
+
+/* ********************** Sub-Program Section Start ************* */
+   int main()
+{
+    signed char x = -6;
+    signed char y;
+    y = x >> 2;
+    printf("%i",y);
+    return 0;
+}
+/* ********************** Sub-Program Section End ************* */
+
+/**
+ ******************************************************************************
+ User          Date                 Brief
+ ******************************************************************************
+ ola ahmed      29jan2024           second task a
+*/
